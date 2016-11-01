65fee586cb5da7ca4a8f695163aa12145b301980
180579045 <msn103125@126.com>
2016-11-01 17:57:27 +0800

    Add some Delegate code

--- LearnDepthChapterTwo/Program.cs
+++ LearnDepthChapterTwo/Program.cs
@@ -8,22 +8,29 @@ namespace LearnDepthChapterTwo
 {
     // 声明了一个委托;
     delegate void StringProcessor(string str);
+    delegate void StringProcessor2(string str);
 
     class Program
     {
         static void Main(string[] args)
         {
             // 创建委托类型的实例;
+            // 将委托实例就相当于创建了一个属性，这个属性可以为起“赋值”，即订阅对应的方法，且可以订阅多个方法
             StringProcessor printstring;
+            StringProcessor2 printstring2;
             // 创建委托实例，使用静态方法;
             printstring = new StringProcessor(Program.PrintString);
+            printstring2 = new StringProcessor2(Program.PrintString);
+            printstring2 += Program.PrintString2;
             printstring("Delegate~printstring");
+            printstring2("Delegate~printstring2");
 
             // 创建委托类型的实例;这个对象被称为操作的目标
             StringProcessor printstringinstance;
             // 创建委托实例，使用实例方法;
             ClassChapter2Instance C2 = new ClassChapter2Instance();
             printstringinstance = new StringProcessor(C2.PrintStringInstance);
+            printstringinstance += C2.PrintStringInstance2;
             printstringinstance("Delegate~printstringinstance");
 
             Console.ReadLine();
@@ -35,7 +42,12 @@ namespace LearnDepthChapterTwo
         // 一个符合委托声明的函数实现;
         static void PrintString(string str)
         {
-            Console.WriteLine("PrintString:" + str);
+            Console.WriteLine("顺序执行静态方法PrintString第一次:" + str);
+        }
+
+        static void PrintString2(string str)
+        {
+            Console.WriteLine("顺序执行静态方法PrintString第二次:" + str);
         }
     }
 
@@ -43,7 +55,12 @@ namespace LearnDepthChapterTwo
     {
         public void PrintStringInstance(string str)
         {
-            Console.WriteLine("PrintStringInstance:" + str);
+            Console.WriteLine("执行实例化方法PrintStringInstance第一次:" + str);
+        }
+
+        public void PrintStringInstance2(string str)
+        {
+            Console.WriteLine("执行实例化方法PrintStringInstance第二次:" + str);
         }
     }
 }
