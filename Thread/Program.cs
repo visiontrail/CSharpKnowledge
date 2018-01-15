e6a36abdc357034a1a0dac2d0fa61f1e0f6cea7a
Guoliang3 <msn103125@126.com>
2018-01-15 20:02:06 +0800

    add Monitor and lock

--- Thread/Program.cs
+++ Thread/Program.cs
@@ -12,19 +12,25 @@ namespace ThreadLearn
         static void Main(string[] args)
         {
 
-            //                                                           静态TLS和动态TLS
-            //---------------------------------------------------------------------------
-            ThreadBasic myStaticTest = new ThreadBasic();                     // 实例化一个对象;
-            ThreadStart ts1 = new ThreadStart(myStaticTest.AddXY);
+            //                                                                      静态TLS和动态TLS
+            //--------------------------------------------------------------------------------------
+            ThreadBasic_DyTLS DTLSData = new ThreadBasic_DyTLS();              // 实例化一个对象;
+            LocalDataStoreSlot slot = Thread.AllocateNamedDataSlot("slot");    // 设置一个有名称的槽;
 
-            LocalDataStoreSlot slot = Thread.AllocateNamedDataSlot("slot1");  // 设置一个有名称的槽;
+            Thread Dt1 = new Thread(() => { Thread.SetData(slot, 10); DTLSData.SetDyThreadData(); });
+            Thread Dt2 = new Thread(() => { Thread.SetData(slot, 20); DTLSData.SetDyThreadData(); });
+
+            Dt1.Start();
+            Dt1.Join();
+            Console.WriteLine("动态TLS，用槽赋值:" + DTLSData.threadX);
+            Dt2.Start();
+            Dt2.Join();
+            Console.WriteLine("动态TLS，用槽赋值:" + DTLSData.threadX);
             
-            Thread.SetData(slot, 10);                                         // 设置这个槽中的值;
-            Thread Dt1 = new Thread(ts1);
-
-            Console.WriteLine("动态TLS：");
-
             Console.ReadLine();
+            
+            ThreadBasic_StaticTLS StaticTLSData = new ThreadBasic_StaticTLS();                     // 实例化一个对象;
+            ThreadStart ts1 = new ThreadStart(StaticTLSData.AddXY);
 
             Thread t1 = new Thread(ts1);
             Thread t2 = new Thread(ts1);
@@ -38,10 +44,21 @@ namespace ThreadLearn
             t4.Start();
             t5.Start();
 
-            //string info = string.Format("非线程私有的X={0}",ThreadBasic_ThreadStaticArr.X);
-            //Console.WriteLine(info);
+            Thread.Sleep(20);
             
             Console.ReadLine();
+
+            ThreadMonitor a = new ThreadMonitor();
+            
+            Thread Mt1 = new Thread(a.VisitObjMonitor);
+            Thread Mt2 = new Thread(a.VisitObjLock);
+
+            Mt1.Start();
+            Thread.Sleep(10);
+            Mt2.Start();
+
+            Console.ReadLine();
+
         }
     }
 }
--- Thread/ThreadBasic.cs
+++ Thread/ThreadBasic.cs
@@ -7,29 +7,105 @@ using System.Threading;
 
 namespace ThreadLearn
 {
-    class ThreadBasic
+    class ThreadBasic_DyTLS
     {
         public static int X = 0;                 // 处于共享状态的静态类型X
         [ThreadStatic]
         public static int threadStaticX = 0;     // 每个线程都拥有自己副本的threadStaticX
         public int threadX = 0;
 
+        public void SetDyThreadData()
+        {
+            LocalDataStoreSlot dataslot = Thread.GetNamedDataSlot("slot");
+            threadX = (int)Thread.GetData(dataslot);
+            
+        }
+
+    }
+
+    class ThreadBasic_StaticTLS
+    {
+        public static int X = 0;                 // 处于共享状态的静态类型X
+        [ThreadStatic]
+        public static int threadStaticX = 0;     // 每个线程都拥有自己副本的threadStaticX
+
         public void AddXY()
         {
-            for (int i = 0; i < 10000; i++)
+            for (int i = 0; i < 200; i++)
             {
                 X++;
                 threadStaticX++;
 
                 Thread current = Thread.CurrentThread;
 
-                string info = string.Format("threadID:{0} 非线程私有的X={1}; [ThreadStatic]线程私有的threadStaticX={2}", 
+                string info = string.Format("threadID:{0} 非线程私有的X={1}; [ThreadStatic]线程私有的threadStaticX={2}",
                                             current.ManagedThreadId, X, threadStaticX);
                 Console.WriteLine(info);
             }
+        }
+    }
 
+    class ThreadMonitor
+    {
+        static object obj_Monitor = new object();
+        static int obj_Int = 10;
+
+        public void VisitObjMonitor()
+        {
+            Monitor.Enter(obj_Monitor);
+
+            try
+            {
+                // DoSomething so long about 10s
+                Console.WriteLine("Do Something about 5s by VisitObjMonitor");
+                Thread.Sleep(5000);
+            }
+            finally
+            {
+                // 只要Enter方法成功了，那么总会调用Exit();
+                Monitor.Exit(obj_Monitor);
+            }
+        }
+
+        public void VisitObjLock()
+        {
+            lock(obj_Monitor)
+            {
+                // DoSomething
+                Console.WriteLine("Do Something Critical Zone VisitObjLock");
+            }
+        }
+
+        public void VisitValueMonitor()
+        {
+            Monitor.Enter(obj_Int as object);
+
+            lock(obj_Int as object)
+            {
+
+            }
+        }
+
+        public void VisitObjMonitor_TryEnter()
+        {
+            while (!Monitor.TryEnter(obj_Monitor))
+            {
+                // 尝试进入的时候，可以干掉别的，比如打印个等待符号什么的;
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+            try
+            {
+                // 执行临界操作;
+                Console.WriteLine();
+                Console.WriteLine("Do Something Critical Zone VisitObjMonitor_TryEnter");
+            }
+            finally
+            {
+                Monitor.Exit(obj_Monitor);
+            }
         }
 
     }
-    
+
 }
