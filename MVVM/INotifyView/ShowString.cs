561d351034413b4a4edde2d2ca2dc7b36cc4da87
Guoliang <msn103125@126.com>
2018-04-09 22:40:42 +0800

    update MVVM Learning

---
+++ MVVM/INotifyView/ShowString.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Threading;
+
+namespace INotifyView
+{
+    public class ShowString : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _name;
+        public string m_Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged("m_Name");
+            }
+        }
+       
+        public void RaisePropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        public ShowString()
+        {
+            this.m_Name = "1111";
+        }
+    }
+}
