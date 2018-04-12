e31ccffce704d4d84bff55125eeb56b7644e6cfa
Guoliang <msn103125@126.com>
2018-04-13 01:43:52 +0800

    update data binding

--- MVVM/INotifyView/MainWindow.xaml.cs
+++ MVVM/INotifyView/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,17 +21,37 @@ namespace INotifyView
     /// </summary>
     public partial class MainWindow : Window
     {
-        //public ShowString PageMain = new ShowString();
+        public ShowString PageMain = new ShowString();
+        
         public MainWindow()
         {
             InitializeComponent();
-            //DataContext = PageMain;       // 通过这行语句绑定前端与后台的关系;
+            // 通过这行语句绑定前端与后台的关系;
+            DataContext = PageMain;
+            // 还可以通过以下单独绑定的方式进行前后台的关联;
+            this.Birthday.SetBinding(TextBox.TextProperty, new Binding("m_Birthday") { Source = PageMain });
+
+            Task a = new Task(() =>
+            {
+                while(true)
+                {
+                    Thread.Sleep(1000);
+                    PageMain.m_Birthday = DateTime.Now.ToString();
+                }
+            });
+
+            a.Start();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            this.text.Text = "Modify the Text";
+            //this.text.Text = "Modify the Text";
             PageMain.m_Name = "222";
+
+            // 尝试更换另一个绑定对象;
+            ShowString dt2 = new ShowString();
+            this.Birthday.SetBinding(TextBox.TextProperty, new Binding("m_Birthday") { Source = dt2 });
+            dt2.m_Birthday = DateTime.UtcNow.ToString();
         }
     }
 
--- MVVM/INotifyView/ShowString.cs
+++ MVVM/INotifyView/ShowString.cs
@@ -19,13 +19,34 @@ namespace INotifyView
             {
                 return _name;
             }
+            // 当m_Name发生变化的时候，PropertyChanged就会被激发,以通知UI层;
             set
             {
                 _name = value;
                 RaisePropertyChanged("m_Name");
             }
         }
+
+        private string _brithday;
+        public string m_Birthday
+        {
+            get
+            {
+                return _brithday;
+            }
+            set
+            {
+                _brithday = value;
+                RaisePropertyChanged("m_Birthday");
+            }
+        }
        
+        /// <summary>
+        /// 通知给UI层，哪个属性发生了变化;
+        /// 当然，这个函数也可以独立出来，让所有想要称为VM层的类型继承，这样就不用重复写多次了;
+        /// 这种绑定的方式有个缺点，就是它通过字符串索引，这样一来，首先不能包含重复的名称了，再有如果写错，也就绑定不上了;
+        /// </summary>
+        /// <param name="propertyName">改变了数值的属性</param>
         public void RaisePropertyChanged(string propertyName)
         {
             if (PropertyChanged != null)
