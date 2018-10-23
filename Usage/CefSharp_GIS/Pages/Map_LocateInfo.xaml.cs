3d2e306da6c23730b9ce59d4ccbfc1136fd8d8c7
visiontrail <msn103125@126.com>
2018-10-23 17:33:35 +0800

    come true CefSharp:WPF -> Chrome

---
+++ Usage/CefSharp_GIS/Pages/Map_LocateInfo.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CefSharp;
+using CefSharp.Wpf;
+
+namespace CefSharp_GIS.Pages
+{
+    /// <summary>
+    /// 一个在WPF程序中调用Chrome内核，显示WebGIS的实验小程序;
+    /// </summary>
+    public partial class Map_LocateInfo : UserControl
+    {
+        MapLocationViewModel m_TransmitData = new MapLocationViewModel();
+
+        public Map_LocateInfo()
+        {
+            InitializeComponent();
+            this.address.Address = System.Environment.CurrentDirectory + @"\ViewPage\pages\AboutLeafLet.html";
+            CefSharp.CefSharpSettings.LegacyJavascriptBindingEnabled = true;
+
+            m_TransmitData.m_Latitude = "39.923428952672154";
+            m_TransmitData.m_Longitude = "116.38778686523436";
+
+            this.address.RegisterJsObject("JsObj", m_TransmitData);        // 向前端页面注册一个JsObj，前端可以通过这个进行交互;
+            this.address.BeginInit();                                      // 刷新页面,以便让数据传送到前端;
+        }
+
+        /// <summary>
+        /// 显示Chrome浏览器内核的调试界面;
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.address.ShowDevTools();
+        }
+
+        private void ReloadPage(object sender, RoutedEventArgs e)
+        {
+            this.address.Reload();
+        }
+    }
+
+
+}
---
+++ Usage/CefSharp_GIS/WebGISDataModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CefSharp_GIS
+{
+    /// <summary>
+    /// 这个数据模型，和前端页面的数据模型保持一致;
+    /// </summary>
+    public class WebGISDataModel
+    {
+        public string LocationList { get; set; }
+        public int MapLevel { get; set; }
+    }
+
+    /// <summary>
+    /// WPF端的内部数据模型;
+    /// </summary>
+    public class MapLocationViewModel
+    {
+        // 测试数据经纬度：39.923428952672154,116.38778686523436;
+        private string TransmitData = FromDataToJson();
+        public string m_Longitude { get; set; }
+        public string m_Latitude { get; set; }
+        public int m_MapLevel { get; set; }
+        
+        /// <summary>
+        /// 初始化数据，并将数据转化为JSON格式;
+        /// </summary>
+        /// <returns></returns>
+        private static string FromDataToJson()
+        {
+            List<string> LocationList = new List<string>();
+            int MapLevel = 10;
+            WebGISDataModel data = new WebGISDataModel();
+            data.MapLevel = MapLevel;
+            
+            return ObjectToJson(data);
+        }
+
+        public static string ObjectToJson(object obj)
+        {
+            return JsonConvert.SerializeObject(obj);
+        }
+
+        public string m_TransmitData
+        {
+            get
+            {
+                return TransmitData;
+            }
+            set
+            {
+                TransmitData = value;
+            }
+        }
+    }
+
+}
