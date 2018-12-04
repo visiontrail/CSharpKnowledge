cd324b1405960bddec45807b27d301a67f429aca
visiontrail <msn103125@126.com>
2018-12-04 10:53:24 +0800

    Generic feature experiment for LNode

---
+++ ADO/ADO_UI_Console/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.OleDb;
+
+namespace ADO_UI_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = string.Format("provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=Excel 8.0;", @"C:\Users\Datang\Documents\人事数据库2.xlsx");
+
+
+            DataSet data = new DataSet();
+
+            foreach (var sheetName in GetExcelSheetNames(connectionString))
+            {
+                using (OleDbConnection con = new OleDbConnection(connectionString))
+                {
+                    var dataTable = new DataTable();
+                    string query = string.Format("SELECT * FROM [{0}]", sheetName);
+                    con.Open();
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(query, con);
+                    adapter.Fill(dataTable);
+                    data.Tables.Add(dataTable);
+                }
+            }
+        }
+
+        static string[] GetExcelSheetNames(string connectionString)
+        {
+            OleDbConnection con = null;
+            DataTable dt = null;
+            con = new OleDbConnection(connectionString);
+            con.Open();
+            dt = con.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, null);
+
+            if (dt == null)
+            {
+                return null;
+            }
+
+            String[] excelSheetNames = new String[dt.Rows.Count];
+            int i = 0;
+
+            foreach (DataRow row in dt.Rows)
+            {
+                excelSheetNames[i] = row["TABLE_NAME"].ToString();
+                i++;
+            }
+
+            return excelSheetNames;
+        }
+    }
+}
---
+++ DataStructure/LinkNode/LNode.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkNode
+{
+    /// <summary>
+    /// 抽象出链表指向Next的节点;
+    /// 这样就可以保证一串链表中的每一个数据都可以是不同的类型了;
+    /// 原因：TypeLNode<T>都被看做是LNode;
+    /// </summary>
+    public class LNode
+    {
+        protected LNode m_Next;
+
+        public LNode(LNode next_node)
+        {
+            m_Next = next_node;
+        }
+    }
+
+    /// <summary>
+    /// 一个泛型类型的链表;
+    /// 他的好处是：不限制链表中类型的种类，可以在数据中填充任意类型;
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class TypeLNode<T> : LNode
+    {
+        public T m_data;
+        public TypeLNode(T data) : this(data, null)
+        {
+        }
+
+        /// <summary>
+        /// 创建一个链表中的一个节点，需要它本身的数据和它的下一条数据;
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="next_node"></param>
+        public TypeLNode(T data, LNode next_node) : base(next_node)
+        {
+            m_data = data;
+        }
+    }
+}
