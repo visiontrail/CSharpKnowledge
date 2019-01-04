ac6b682fb4250fcfa5bb90d9468c251acda0eaa9
visiontrail <msn103125@126.com>
2019-01-04 19:32:41 +0800

    udpate

--- UnitTestForAllProject/CefSharpGIS/UnitTest_MapLocationViewModel.cs
+++ UnitTestForAllProject/CefSharpGIS/UnitTest_MapLocationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿// 在文件头中引用要被测试的项目;
+using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CefSharp_GIS;
 
@@ -7,7 +8,6 @@ namespace UnitTestForAllProject
     [TestClass]
     public class UnitTest_MapLocationViewModel
     {
-
         MapLocationViewModel mvm = new MapLocationViewModel();
 
         [TestMethod]
@@ -15,6 +15,8 @@ namespace UnitTestForAllProject
         {
             Console.WriteLine("运行单元测试:" + mvm.m_TransmitData);
             string ret = @"{'LocationList':null,'MapLevel':10,'Longtitude':'116.38778686523436','Latitude':'39.923428952672154'}";
+
+            // 判断是否符合预期;
             Assert.AreEqual(ret, mvm.m_TransmitData);
         }
     }
